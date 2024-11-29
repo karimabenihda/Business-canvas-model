a0182d2243d54bd32bcf5d8bc23929935a128c1f
Laila Elmallass <Laila.elmallass.2018@gmail.com>
2024-11-29 11:42:22 +0100

    2nd commit

--- src/BmcComponent.jsx
+++ src/BmcComponent.jsx
@@ -15,12 +15,17 @@ const BusinessModelCanvas = () => {
     revenueStreams: [],
   });
 
+  const [editingSection, setEditingSection] = useState(null); // Track which section is being edited
+
   const handleAddItem = (section, item) => {
     setSections((prev) => ({ ...prev, [section]: [...prev[section], item] }));
   };
 
   const handleRemoveItem = (section, index) => {
-    setSections((prev) => ({ ...prev, [section]: prev[section].filter((_, i) => i !== index) }));
+    setSections((prev) => ({
+      ...prev,
+      [section]: prev[section].filter((_, i) => i !== index),
+    }));
   };
 
   return (
@@ -34,7 +39,9 @@ const BusinessModelCanvas = () => {
       <div className="canvas-sections">
         {Object.keys(sections).map((section) => (
           <div key={section} className="box">
-            <h2 className="title">{section}</h2>
+            <h2 className="title" onClick={() => setEditingSection(section)}>
+              {section}
+            </h2>
             <ul className="list">
               {sections[section].map((item, index) => (
                 <li key={index} className="item">
@@ -45,17 +52,21 @@ const BusinessModelCanvas = () => {
                 </li>
               ))}
             </ul>
-            <input
-              type="text"
-              className="input"
-              placeholder={`Add ${section}`}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  handleAddItem(section, e.currentTarget.value);
-                  e.currentTarget.value = '';
-                }
-              }}
-            />
+
+            {/* Show the input field only when the section is clicked */}
+            {editingSection === section && (
+              <input
+                type="text"
+                className="input"
+                placeholder={`Add ${section}`}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    handleAddItem(section, e.currentTarget.value);
+                    e.currentTarget.value = '';
+                  }
+                }}
+              />
+            )}
           </div>
         ))}
       </div>
